f3700c64c8a99f1e4f597b821f55346b57097c5b
VazhaBurduli <121959221+VazhaBurduli@users.noreply.github.com>
2023-03-01 14:07:55 +0400

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        768:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
